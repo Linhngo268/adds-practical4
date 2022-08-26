edc267e5702f382577e7d1d4748f683ac627c514
Linhngo268 <83965979+Linhngo268@users.noreply.github.com>
2022-08-26 16:57:32 +0930

    Update Reverse.cpp

--- Reverse.cpp
+++ Reverse.cpp
@@ -1,26 +1,24 @@
  
 #include "Reverse.h"
 #include <string>
-using namespace std;
+ 
 
-
-std::string Reverse::reverseString(std::string str1)//reverse string recursion
+std::string Reverse::reverseString(std::string letters)//reverse string recursion
 
        {
 
-       size_t number_characters= str1.size();
+       size_t number_characters= letters.size();
 
      
 
        if(number_characters== 1)
 
-       return str1;
+       return letters;
 
        else
 
        {
-return str1[number_characters-1]+
-           reverseString(str1.substr(0, number_characters- 1));
+return letters[number_characters-1]+ reverseString(letters.substr(0, number_characters- 1));
 
        }
 
@@ -30,23 +28,23 @@ return str1[number_characters-1]+
 
 
 
- int Reverse::reverse_Digit(int val)
+ int Reverse::reverse_Digit(int value)
 
        {
 
-           if(val<10){
-               return val;
+           if(value<10){
+               return value;
                }
 
        int len=0;
 
-       for (int i=val; i; i/=10){
+       for (int i=value; i; i/=10){
            len++;
            }
 
      
 
-       return val%10*(int)pow(10, len-1) + reverse_Digit(val/10);
+       return value%10*(int)pow(10, len-1) + reverse_Digit(value/10);
 
        }
 
